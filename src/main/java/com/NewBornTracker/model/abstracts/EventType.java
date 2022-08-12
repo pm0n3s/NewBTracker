2ca3f1b63f9294d3b22839ebafa791a7076eef27
Bodin <patrick.bodin@accenturefederal.com>
2022-08-12 12:36:18 -0400

    initial task3 commit

--- src/main/java/com/NewBornTracker/model/abstracts/EventType.java
+++ src/main/java/com/NewBornTracker/model/abstracts/EventType.java
@@ -3,7 +3,6 @@ package com.NewBornTracker.model.abstracts;
 import javax.persistence.DiscriminatorColumn;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
